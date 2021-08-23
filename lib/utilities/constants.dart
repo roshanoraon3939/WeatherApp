221f0920f99096d0c9af25f8cb23f3fdb772d0d0
roshanoraon3939 <roshanoraon3939@gmail.com>
2021-08-23 13:33:37 +0530

    add

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Bangers',
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kInTextStyle =TextStyle(
+    fontSize: 50.0,
+    fontFamily: 'Knewave'
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 70.0,
+);
+
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10.0),
+    ),
+    borderSide: BorderSide.none,
+  ),
+);
